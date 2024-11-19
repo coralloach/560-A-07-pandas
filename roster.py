711f71b662c62faab7758586b6448a8c7a2e8511
Cora1337 <coralloach@gmail.com>
2024-11-19 00:47:14 -0500

    different method of formatting bmi

--- roster.py
+++ roster.py
@@ -12,14 +12,16 @@ data = pd.DataFrame(player)
 
 # To get the bmi down to 2 decimal places, I asked chatgpt, it gave me several options
 # I did try using round() to format the values as they are assigned to the data frame
-# data["bmi"] = round((data["weight"]/2.205)/((data["height"]/39.37)**2), 2)
-data["bmi"] = (data["weight"]/2.205)/((data["height"]/39.37)**2)
 
-# The second option is the one I went with, it formats during printing, rather than
-# modifying the data frame
-# I like it because it looks pretty clean in the code, rather than hiding in a long line,
-# like the first option
-print(data.round({"bmi": 2}))
+# I initially used another method, but because they modified the print, rather
+# Than the data frame, the csv file still had six decimal places
+# By using the method that alters the data frame, the csv data appears in the desired format
+
+data["bmi"] = round((data["weight"]/2.205)/((data["height"]/39.37)**2), 2)
+
+# The second option formats during printing, rather than modifying the data frame
+# print(data.round({"bmi": 2}))
+print(data)
 
 data.to_csv("bmi.csv")
 
@@ -27,3 +29,5 @@ data.to_csv("bmi.csv")
 # data["bmi"] = data["bmi"].apply(lambda x: f"{x:.2f}")
 # Which I chose not to use, because I am not fully sure how it works
 # It was labeled as formatting the string during printing
+# I also got another option that seems to do the same as option three
+# data["bmi"] = data["bmi"].round(2)
